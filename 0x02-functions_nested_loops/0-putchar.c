4bb244e3ff34a86578f9eac8d28b26b3775973a5
Benjaminikpang <benjaminikpang@gmail.com>
2023-02-21 16:36:18 -0800

    nested

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - _putchar - writes the character c to stdout
+ * Return: Always 0, if successful
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
