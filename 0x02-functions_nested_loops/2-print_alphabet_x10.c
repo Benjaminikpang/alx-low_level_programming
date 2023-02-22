3c8ee0f87e430d88b3f00f615da133e5b0bfe0e2
Benjaminikpang <benjaminikpang@gmail.com>
2023-02-21 18:09:08 -0800

    nested

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet in lowercase
+ * Return:0
+ */
+void print_alphabet_x10(void)
+{
+	char (c = 'a';
+
+	i = '0');	
+	while (c <= 'z');
+		     
+	(i < '10');
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	i++;
+}
