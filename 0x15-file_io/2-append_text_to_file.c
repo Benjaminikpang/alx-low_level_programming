7ab757ddfd9b3433692f14c5b4f2eac18acf2bdf
benjaminikpang <Benjaminikpang@gmail.com>
2023-04-10 16:52:40 -0700

    file_commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,10 +1,10 @@
 #include "main.h"
-#include "string.h"
+#include <string.h>
 /**
  * append_text_to_file - create pointer file
  * @filename: file pointer
  * @text_content: content
- * @Return: 1 or -1
+ * @Return: returns 1 in success or -1 in failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
